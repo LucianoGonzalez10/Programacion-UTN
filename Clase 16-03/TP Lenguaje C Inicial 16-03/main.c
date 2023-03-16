417eeafae2b109d290cb29bd1cedc9dc8acf3494
LucianoGonzalez10 <90476857+LucianoGonzalez10@users.noreply.github.com>
2023-03-16 10:26:02 -0300

    Update main.c

--- Clase 16-03/TP Lenguaje C Inicial 16-03/main.c
+++ Clase 16-03/TP Lenguaje C Inicial 16-03/main.c
@@ -3,21 +3,59 @@
 
 int main()
 {
-   /*               (Ejercicio 1)
+   /*            // "Problemas secuenciales" EJERCICIO 1
    int capital;
    float intereses = 0.02;
    float ganancia;
    printf ("Ingrese su dinero a invertir ");
    scanf ("%i", &capital);
    ganancia=((float)capital*intereses);
-   printf ("Su ganancia es de: $%.0f  ",ganancia); */
+   printf ("Su ganancia es de: $%.0f  ",ganancia);
 
-                    //(Ejercicio 2)
 
-   float descuento=0.15,total;
+                // EJERCICIO 2
+   float descuento = 0.15, total;
    int precio;
-   printf ("Ingrese el monto de su compra:\n");
+
+   printf("Ingrese el precio: \n");
    scanf("%i", &precio);
-   total=((float)precio*descuento);
+   total = ((float) precio - (float)precio*descuento);
+   printf("El total es de $%.0f \n", total);
+
+
+
+                 // EJERCICIO 3
+
+    int hombres, mujeres, total;
+    float porcentajeMuj, porcentajeHom;
+    printf("Ingrese la cantidad de mujeres \n");
+    scanf("%i", &mujeres);
+
+    printf("Ingrese la cantidad de hombres \n");
+    scanf("%i", &hombres);
+
+    total = (mujeres + hombres);
+    printf ("El total de alumnos es: %i \n", total);
+
+
+    porcentajeHom = ((float)hombres/(float) total)*100;
+    printf("%.0fporciento de hombres \n", porcentajeHom);
+
+    porcentajeMuj = ((float)mujeres/(float) total)*100;
+    printf("%.0fporciento de mujeres \n", porcentajeMuj);
+
+
+           // "Condicionales selectivos simples" EJERCICIO 1
+    int notas,suma=0,i;
+    float promedio;
+    for (i=0; i<3;i++)
+        {
+        printf ("Ingrese su nota: \n");
+        scanf ("%i",&notas);
+        suma=(notas+suma);
+        }
+        promedio=((float)suma/3);
+    printf("El promedio es: %.1f", promedio);
+    */
    return 0;
 }
